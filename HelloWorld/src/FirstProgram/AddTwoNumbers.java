6e8d5b152de0cb6dc7d4235065bfbf8c7ea0b7e5
Murugananthan <PMurugananth@ad.harman.com>
2017-11-03 11:25:31 +0530

    Adding two numbers

---
+++ HelloWorld/src/FirstProgram/AddTwoNumbers.java
@@ -0,0 +1,10 @@
+package FirstProgram;
+public class AddTwoNumbers {
+ 
+	public static void main(String [] args){
+		int a = 9; 
+		int b = 21;
+		int sum = b-(-a);
+		System.out.println(sum);
+}
+}
